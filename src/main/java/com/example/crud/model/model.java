611ef07a62ff4b053ff610edd6b67bbb7a6968c0
Shash926 <shashankgss19@gmail.com>
2024-09-30 12:19:30 +0530

    model class updated
    model class updated

--- src/main/java/com/example/crud/model/model.java
+++ src/main/java/com/example/crud/model/model.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 //import lombok.Data;
-
+//this is model
 //@Data
 @Entity
 @Table(name = "COURSE_DTLS")
